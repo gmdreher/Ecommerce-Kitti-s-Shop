6f2d2dbbb7f6dcc7b909c0bdb3dda9c67513960d
mar156 <m.n.v_156@hotmail.com.ar>
2021-01-22 12:22:31 -0300

    stasheado

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,5 +1,6 @@
 const server = require('express').Router();
 const { Product } = require('../db.js');
+const Category = require('../models/Category.js');
 
 server.get('/', (req, res, next) => {
 	Product.findAll()
@@ -7,6 +8,30 @@ server.get('/', (req, res, next) => {
 			res.send(products);
 		})
 		.catch(next);
+});
+
+server.post('/:idProducto/category/:idCategory', (req, res) => {
+	const {idProducto, idCategory} = req.params;
+	
+	Product.update({categoryId: idCategory}, {where: {
+			idProducto:idProducto
+		}}
+		
+	).then((product) => {
+		res.json(product);
+	})
+	.catch((err) => {
+		return res.send({ data: err }).status(400);
+})
+})
+
+server.delete('/:idProducto/category/:idCategoria', (req, res) => {
+	const {idProducto, idCategoria} = req.params;
+		Product.find({where:{productId:idProducto}})
+		.then((product)=>{
+			product.destroy(idCategoria)
+		})
+		.catch(error=>res.send(error))
 });
 
 module.exports = server;
