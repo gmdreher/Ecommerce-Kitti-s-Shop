83deee43652701c75ef95fdc7335ba6cd0d2b36f
Angelismar Magallanes <angelis732@gmail.com>
2021-02-03 09:28:24 -0300

    Cambien de vuelta el product module que le habia colocado a solo scss, porque se dañaba todo.

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import './Product.module.scss';
+import './Product.scss';
 import "bootstrap/dist/css/bootstrap.min.css";
  import { useDispatch, useSelector } from 'react-redux';
 import { getProductById} from '../../actions/productActions.js';
@@ -13,6 +13,7 @@ function Product(props) {
     useEffect(() => {
         props.getProductById(props.id);
     }, [])
+    
     let imageUrl;
     if (props.product.images) {
         imageUrl = props.product.images[0].url;
