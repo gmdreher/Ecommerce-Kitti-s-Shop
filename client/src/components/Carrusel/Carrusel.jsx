926016b6fb4439e07734622e47b0b28ef17ef4a0
Mara Bayurk <70042993+MaraBayurk@users.noreply.github.com>
2021-02-15 18:40:36 -0300

    en el carrusel salen los descuentos disponibles

--- client/src/components/Carrusel/Carrusel.jsx
+++ client/src/components/Carrusel/Carrusel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from './carrusel.module.scss';
 import {
@@ -8,6 +8,8 @@ import {
   CarouselIndicators,
   CarouselCaption
 } from 'reactstrap';
+import { getDiscountActive } from '../../actions/discountsActions';
+import { useDispatch, useSelector } from 'react-redux';
 
 const items = [
   {
@@ -30,6 +32,28 @@ const items = [
 const Example = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [animating, setAnimating] = useState(false);
+  const dispatch = useDispatch()
+  const discounts = useSelector((state)=>state.auth.discounts)
+  const [showDiscounts, setShowDiscounts] = useState([])
+
+  useEffect(()=>{
+    dispatch(getDiscountActive())
+  },[])
+
+  useEffect(()=>{
+    if(discounts == undefined || discounts.length <1)return
+    let aux =discounts.sort((a, b)=> {
+      if (a.percentage < b.percentage) {
+          return 1;
+      }
+      if (a.percentage > b.percentage) {
+          return -1;
+      }
+      // a must be equal to b
+      return 0;
+  }).slice(0,3)
+    setShowDiscounts(aux)
+  },[discounts])
 
   const next = () => {
     if (animating) return;
@@ -48,7 +72,7 @@ const Example = () => {
     setActiveIndex(newIndex);
   }
 
-  const slides = items.map((item) => {
+  const slides = items.map((item,indice) => {
     return (
 
       <CarouselItem
@@ -56,8 +80,16 @@ const Example = () => {
         onExiting={() => setAnimating(true)}
         onExited={() => setAnimating(false)}
         key={item.src}
-      >
+        className={styles.prueba}
+        >
         <img src={item.src} alt={item.altText} />
+        {showDiscounts[indice] &&
+        <div className={styles.contentDiscount}>
+          <h2 >HOY DESCUENTO {showDiscounts[indice].percentage} %</h2>
+          <h4>En compras mayores a ${showDiscounts[indice].mount}</h4>
+
+        </div>
+        }
         <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
       </CarouselItem>
 
@@ -82,123 +114,6 @@ const Example = () => {
 }
 
 export default Example;
-
-
-
-
-
-
-//************************************************************* */
-// import React, { useEffect, useState } from 'react';
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import styles from './carrusel.module.scss';
-// import {
-//   Carousel,
-//   CarouselItem,
-//   CarouselControl,
-//   CarouselIndicators,
-//   CarouselCaption
-// } from 'reactstrap';
-// import { getDiscountActive } from '../../actions/discountsActions';
-// import { useDispatch, useSelector } from 'react-redux';
-
-// const items = [
-//   {
-//     src: require('../../img/21.jpg'),
-//     altText: 'Gatito',
-
-//   },
-//   {
-//     src: require('../../img/22.jpg'),
-//     altText: 'Slide 2',
-
-//   },
-//   {
-//     src: require('../../img/24.jpg'),
-//     altText: 'Slide 3',
-
-//   }
-// ];
-
-// const Example = () => {
-//   const [activeIndex, setActiveIndex] = useState(0);
-//   const [animating, setAnimating] = useState(false);
-//   const dispatch = useDispatch()
-//   const discounts = useSelector((state)=>state.auth.discounts)
-//   const [showDiscounts, setShowDiscounts] = useState([])
-
-//   useEffect(()=>{
-//     dispatch(getDiscountActive())
-//   },[])
-
-//   useEffect(()=>{
-//     if(discounts == undefined || discounts.length <1)return
-//     let aux =discounts.sort((a, b)=> {
-//       if (a.percentage < b.percentage) {
-//           return 1;
-//       }
-//       if (a.percentage > b.percentage) {
-//           return -1;
-//       }
-//       // a must be equal to b
-//       return 0;
-//   }).slice(0,3)
-//     setShowDiscounts(aux)
-//   },[discounts])
-
-//   const next = () => {
-//     if (animating) return;
-//     const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-//     setActiveIndex(nextIndex);
-//   }
-
-//   const previous = () => {
-//     if (animating) return;
-//     const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-//     setActiveIndex(nextIndex);
-//   }
-
-//   const goToIndex = (newIndex) => {
-//     if (animating) return;
-//     setActiveIndex(newIndex);
-//   }
-
-//   const slides = items.map((item,indice) => {
-//     return (
-
-//       <CarouselItem
-
-//         onExiting={() => setAnimating(true)}
-//         onExited={() => setAnimating(false)}
-//         key={item.src}
-//         className={styles.prueba}
-//         >
-//         <img src={item.src} alt={item.altText} />
-//         {showDiscounts[indice] &&<h1 >HOY DESCUENTO {showDiscounts[indice].percentage} %</h1>}
-//         <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
-//       </CarouselItem>
-
-//     );
-//   });
-
-//   return (
-//     <div className={styles.mainWelcome}>
-//       <Carousel
-//         activeIndex={activeIndex}
-//         next={next}
-//         previous={previous}
-//       >
-//         <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
-//         {slides}
-//         <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
-//         <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
-//       </Carousel>
-//     </div>
-//   );
-
-// }
-
-// export default Example;
 
 
 
