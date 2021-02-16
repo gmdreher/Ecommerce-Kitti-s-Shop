58b58f24174a818f3773d744044b1c2a04cfe3c9
gmdreher <gmdreher21@gmail.com>
2021-02-16 20:05:09 -0300

    se agrega responsive en Global Discount

--- client/src/components/Discount/GlobalDiscount.js
+++ client/src/components/Discount/GlobalDiscount.js
@@ -75,83 +75,85 @@ export default function GlobalDiscount() {
 
   return (
 
-    <Container className="containerDiscout">
-      <h1>Descuentos</h1>
-      <button className="buttonFormAdd" onClick={toggle}> Agregar Descuento</button>
+    <Container >
+      <h1 className="tituloGlobal">Descuentos</h1>
       <br />
-      <Table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Monto</th>
-            <th>Porcentaje</th>
-            <th>Fecha de creación</th>
-            <th>Duración (días)</th>
-            <th>Estado</th>
-
-          </tr>
-        </thead>
-        <tbody>
-          {descuentos && descuentos.map(((descuento, index) => (
-            <tr key={descuento.id}>
-
-              <td>{descuento.id}</td>
-              <td>{descuento.mount}</td>
-              <td>{descuento.percentage}</td>
-              <td>{formatDate(descuento.createdAt)}</td>
-              <td>{descuento.days}</td>
-              <td>
-                <input checked={descuento.isActive === true ? true : false} onChange={(e) => handleEdit(descuento.id, e.target.checked)} class="form-check-input" type="checkbox" value={descuento.estado} id="flexCheckIndeterminate" />
-              </td>
+      <button className="buttonFormAdd" onClick={toggle}> Agregar Descuento</button>
+      <div className={"table-responsive " + "containerDiscout"}>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Monto</th>
+              <th>Porcentaje</th>
+              <th>Fecha de creación</th>
+              <th>Duración (días)</th>
+              <th>Estado</th>
 
             </tr>
-          )))}
+          </thead>
+          <tbody>
+            {descuentos && descuentos.map(((descuento, index) => (
+              <tr key={descuento.id}>
 
-        </tbody>
-      </Table>
+                <td>{descuento.id}</td>
+                <td>{descuento.mount}</td>
+                <td>{descuento.percentage}</td>
+                <td>{formatDate(descuento.createdAt)}</td>
+                <td>{descuento.days}</td>
+                <td>
+                  <input checked={descuento.isActive === true ? true : false} onChange={(e) => handleEdit(descuento.id, e.target.checked)} class="form-check-input" type="checkbox" value={descuento.estado} id="flexCheckIndeterminate" />
+                </td>
 
-      {/* -------------MODAL POST--------------- */}
-      <div>
-        <Modal isOpen={modal} toggle={toggle} >
-          <Form onSubmit={e => e.preventDefault()}>
-            <ModalHeader toggle={toggle}>Nuevo Descuento</ModalHeader>
-            <ModalBody>
+              </tr>
+            )))}
 
-              <FormGroup>
-                <Label for="monto"> Monto</Label>
-                <Input type="number" name="monto" id='monto' value={input.monto} onChange={handleInputChange} />
+          </tbody>
+        </Table>
 
-              </FormGroup>
+        {/* -------------MODAL POST--------------- */}
+        <div>
+          <Modal isOpen={modal} toggle={toggle} >
+            <Form onSubmit={e => e.preventDefault()}>
+              <ModalHeader toggle={toggle}>Nuevo Descuento</ModalHeader>
+              <ModalBody>
 
-              <FormGroup>
-                <Label for="porcentaje"> Porcentaje</Label>
-                <Input type="number" name="porcentaje" id='porcentaje' value={input.porcentaje} onChange={handleInputChange} />
+                <FormGroup>
+                  <Label for="monto"> Monto</Label>
+                  <Input type="number" name="monto" id='monto' value={input.monto} onChange={handleInputChange} />
 
-              </FormGroup>
-              <FormGroup>
-                <Label for="duracion"> Duración (dias)</Label>
-                <Input type="number" name="duracion" id='duracion' value={input.duracion} onChange={handleInputChange} />
+                </FormGroup>
 
-              </FormGroup>
-              <FormGroup>
-                <Label for="estado"> Estado</Label>
-                <select class="form-select" aria-label="Default select example" name="estado" id="estado" rows="1" value={input.estado} onChange={handleInputChange}>
-                  <option value="false">Inactivo</option>
-                  <option value="true">Activo</option>
-                </select>
-              </FormGroup>
+                <FormGroup>
+                  <Label for="porcentaje"> Porcentaje</Label>
+                  <Input type="number" name="porcentaje" id='porcentaje' value={input.porcentaje} onChange={handleInputChange} />
+
+                </FormGroup>
+                <FormGroup>
+                  <Label for="duracion"> Duración (dias)</Label>
+                  <Input type="number" name="duracion" id='duracion' value={input.duracion} onChange={handleInputChange} />
+
+                </FormGroup>
+                <FormGroup>
+                  <Label for="estado"> Estado</Label>
+                  <select class="form-select" aria-label="Default select example" name="estado" id="estado" rows="1" value={input.estado} onChange={handleInputChange}>
+                    <option value="false">Inactivo</option>
+                    <option value="true">Activo</option>
+                  </select>
+                </FormGroup>
 
 
-            </ModalBody>
-            <ModalFooter>
+              </ModalBody>
+              <ModalFooter>
 
-              <button className="buttonForm" onClick={() => handleAdd({ monto: input.monto, porcentaje: input.porcentaje, duracion: input.duracion, estado: input.estado })} type="submit" >Crear Descuento</button>
+                <button className="buttonForm" onClick={() => handleAdd({ monto: input.monto, porcentaje: input.porcentaje, duracion: input.duracion, estado: input.estado })} type="submit" >Crear Descuento</button>
 
-              <button className="buttonForm" onClick={toggle}>Salir</button>
+                <button className="buttonForm" onClick={toggle}>Salir</button>
 
-            </ModalFooter>
-          </Form>
-        </Modal>
+              </ModalFooter>
+            </Form>
+          </Modal>
+        </div>
       </div>
 
     </Container>
