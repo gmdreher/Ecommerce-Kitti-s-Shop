622ec53531f9fec0e7b4e9e4b81193d977c22234
gmdreher <gmdreher21@gmail.com>
2021-02-17 09:55:31 -0300

    componentes de leo

--- client/src/components/CreateAdoption/AdoptionRequest.js
+++ client/src/components/CreateAdoption/AdoptionRequest.js
@@ -1,93 +1,93 @@
-import React, {Fragment, useEffect, useState,} from "react";
+import React, { Fragment, useEffect, useState, } from "react";
 import { Button, Modal, Form, ModalHeader, ModalBody, ModalFooter, FormGroup, Label, Input, Container, Table } from 'reactstrap';
 import { getAllRequestUser } from '../../actions/adoptionAction';
 import { connect } from 'react-redux';
-import styles from './createAdoption.module.scss'
+import './createAdoption.scss'
 import { Link } from "react-router-dom";
 import Moment from 'moment';
 import { useHistory } from "react-router-dom";
 
 
 
-function AdoptionRequest (props) {
+function AdoptionRequest(props) {
 
   const history = useHistory();
-   //modal agregar reseña
-   const [modal, setModal] = useState(false);
-   const toggleAdd = () => setModal(!modal);
+  //modal agregar reseña
+  const [modal, setModal] = useState(false);
+  const toggleAdd = () => setModal(!modal);
 
-   const [input, setInput] = useState({
+  const [input, setInput] = useState({
     condition: '',
     reason: '',
-    contact:'',
-    province:'',
-    file:''
+    contact: '',
+    province: '',
+    file: ''
   });
   const [errors, setErrors] = useState({}); //---------------------------
 
   const [adoptionStates, setAdoptionStates] = useState('')
 
 
-//validacion inputs
-const validate = function (input) {//---------------------------
-  let errors = {};
-  if (!input.condition) {
-    errors.condition = '**Necesita LLenar este campo';
+  //validacion inputs
+  const validate = function (input) {//---------------------------
+    let errors = {};
+    if (!input.condition) {
+      errors.condition = '**Necesita LLenar este campo';
+    }
+    else
+      if (!input.reason) {
+        errors.reason = '**Necesita LLenar este campo';
+      } else if (!input.contact) {
+        errors.contact = '**Necesita LLenar este campo';
+      } else
+        if (!input.province) {
+          errors.province = '**Necesita LLenar este campo';
+        }
+    return errors;
   }
-  else
-  if (!input.reason) {
-    errors.reason = '**Necesita LLenar este campo';
-  }else if (!input.contact) {
-    errors.contact = '**Necesita LLenar este campo';
-  }else
-  if (!input.province) {
-    errors.province = '**Necesita LLenar este campo';
+
+  //para setear los input
+  const handleInputChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    });
+    setErrors(validate({//---------------------------------------
+      ...input,
+      [e.target.name]: e.target.value
+    }));
+
   }
-  return errors;
-}
+  const resetInput = () => {
+    setInput({
+      condition: '',
+      reason: '',
+      contact: '',
+      province: '',
+      file: ''
+    })
+  }
 
-//para setear los input
-const handleInputChange = (e) => {
-  setInput({
-    ...input,
-    [e.target.name]: e.target.value
-  });
-  setErrors(validate({//---------------------------------------
-    ...input,
-    [e.target.name]: e.target.value
-  }));
-
-}
-const resetInput = () => {
-  setInput({
-    condition: '',
-    reason: '',
-    contact:'',
-    province:'',
-    file:''
-  })
-}
-
- // añadir un review
- const handleOpenModal = () => {
-  // console.log(productId)
- // setIdProductAdding(productId)
-  toggleAdd()
-}
-const handleChangeImage = e => {
-  setInput({
-    ...input,
+  // añadir un review
+  const handleOpenModal = () => {
+    // console.log(productId)
+    // setIdProductAdding(productId)
+    toggleAdd()
+  }
+  const handleChangeImage = e => {
+    setInput({
+      ...input,
       file: e.target.files[0]
       //filename:e.target.files[0].name
-  });
+    });
 
-}
-const handleCreate = e =>{
-  e.preventDefault();
-  props.createAdoption({userId:props.user.id,condition:input.condition,reason:input.reason,contact:input.contact,province:input.province,image:input.file})
-  toggleAdd()
-}
-  
+  }
+  const handleCreate = e => {
+    e.preventDefault();
+    props.createAdoption({ userId: props.user.id, condition: input.condition, reason: input.reason, contact: input.contact, province: input.province, image: input.file })
+    toggleAdd()
+  }
+
   useEffect(() => {
     props.getAllRequestUser(props.user.id)
   }, [])
@@ -96,22 +96,22 @@ const handleCreate = e =>{
     let formatDate = new Moment(date);
     return formatDate.format('DD/MM/YY - HH:mm:ss')
   }
-  
- /*  const filteredOrders = (event) => {
-    props.getAllOrders(event.target.value)
-    setOrderStates(event.target.value)
-  }
-   */
-   
-    return (
-      <Fragment>
-        <div>
-          <button className={styles.button_} onClick={()=>history.push('/users/adoptions')}>Adopciones Creadas</button>
-        </div>
-        <br />
-        <h2 className={styles.title}>Adopciones Solicitadas:</h2>
-        <div className={styles.select}>
-          {/* <div>
+
+  /*  const filteredOrders = (event) => {
+     props.getAllOrders(event.target.value)
+     setOrderStates(event.target.value)
+   }
+    */
+
+  return (
+    <Fragment>
+      <div>
+        <button className="button_adop" onClick={() => history.push('/users/adoptions')}>Adopciones Creadas</button>
+      </div>
+      <br />
+      <h2 className="titleadop">Adopciones Solicitadas:</h2>
+      <div className="select.adop">
+        {/* <div>
             <label>Filtrar por estado </label> &nbsp;
             <select  name="state" id="state" value={orderStates} onChange={filteredOrders}>
               <option value="">Todas</option>
@@ -122,28 +122,28 @@ const handleCreate = e =>{
               <option value="completa">Completas</option>
             </select>
           </div> */}
-        </div>
-        <div className={styles.cont}>
+      </div>
+      <div className="contadop">
         <div className={"table-responsive"}>
-          <table className={"table table-sm " + styles.table} >
+          <table className={"table table-sm " + "table"} >
             <thead>
               <tr>
-                <th className={styles.th} scope="col">Número de Solicitud</th>
-                <th className={styles.th} scope="col">Número de Adopción</th>
-                <th className={styles.th} scope="col">Condiciones Fisicas o de Espacio</th>
-                <th className={styles.th} scope="col">Estado</th>
-                <th className={styles.th} scope="col">Fecha y hora de Actualización</th> 
+                <th className="thadop" scope="col">Número de Solicitud</th>
+                <th className="thadop" scope="col">Número de Adopción</th>
+                <th className="thadop" scope="col">Condiciones Fisicas o de Espacio</th>
+                <th className="thadop" scope="col">Estado</th>
+                <th className="thadop" scope="col">Fecha y hora de Actualización</th>
                 {/* <th className={styles.th} scope="col">Foto</th>  */}
               </tr>
             </thead>
             <tbody >
-        {
+              {
                 props.allRequestUser && props.allRequestUser.map(adopt => {
                   return (
                     <tr key={adopt.id}>
                       <td scope="row" >
                         {/* <Link exact to={`/orders/${order.id}`} > */}
-                          {adopt.id}
+                        {adopt.id}
                         {/* </Link> */}
                       </td>
                       {/* <td>{adopt.userId}</td> */}
@@ -152,12 +152,12 @@ const handleCreate = e =>{
                       </td>
                       <td>{adopt.state}</td>
                       <td>{formatDate(adopt.updatedAt)}</td>
-            
+
                       {/* {adopt.photo?<td><img width='max-width:100%;width:auto' height='auto'src={`data:image/jpg;base64,${adopt.photo}`}/></td>:<td>No Se cargo Imagen</td>} */}
                     </tr>
                   )
                 })
-          } 
+              }
             </tbody>
           </table>
           {/* {
@@ -166,63 +166,63 @@ const handleCreate = e =>{
             </div>: ""
           } */}
         </div>
-        </div>
+      </div>
 
 
 
-        {/* -------------MODAL POST--------------- */}
- <div>
- <Modal isOpen={modal} toggle={toggleAdd} className={props.className}>
-   <Form onSubmit={e => e.preventDefault()}>
-     <ModalHeader toggle={toggleAdd}>Nueva Adopción Responsable</ModalHeader>
-     <ModalBody>
+      {/* -------------MODAL POST--------------- */}
+      <div>
+        <Modal isOpen={modal} toggle={toggleAdd} className={props.className}>
+          <Form onSubmit={e => e.preventDefault()}>
+            <ModalHeader toggle={toggleAdd}>Nueva Adopción Responsable</ModalHeader>
+            <ModalBody>
 
-       <FormGroup onSubmit={e => e.preventDefault()}>
-         <Label for="condition"> Condiciones Fisicas y Descripcion General del Gato(Peso,Edad,Color...)</Label>
-         <Input type="textarea" className={`${errors.condition} && 'danger', "form-group"`} name="condition" id='condition' onChange={handleInputChange} />
-      </FormGroup>                  
-      <FormGroup>
-         <Label for="reason"> Motivo de la entrega</Label>
-         <Input type="textarea" className={`${errors.reason} && 'danger', "form-group"`} name="reason" id='reason'  onChange={handleInputChange} />
-      </FormGroup>
-      <FormGroup>
-         <Label for="contact">Contacto</Label>
-         <Input type="textarea" className={`${errors.contact} && 'danger', "form-group"`} name="contact" id='contact' onChange={handleInputChange} />
-      </FormGroup>
-      <FormGroup>
-         <Label for="province">Provincia Donde se Encuentra</Label>
-         <Input type="textarea" className={`${errors.province} && 'danger', "form-group"`} name="province" id='province' onChange={handleInputChange} />
-      </FormGroup>
-       <FormGroup>
-         <Label><strong>Sube Una Foto Para Que Todos Puedan Verlo!!</strong></Label>
-         <FormGroup>
-          <Input type='file' name='file' onChange={handleChangeImage}/>
-          </FormGroup>
-       </FormGroup>
+              <FormGroup onSubmit={e => e.preventDefault()}>
+                <Label for="condition"> Condiciones Fisicas y Descripcion General del Gato(Peso,Edad,Color...)</Label>
+                <Input type="textarea" className={`${errors.condition} && 'danger', "form-group"`} name="condition" id='condition' onChange={handleInputChange} />
+              </FormGroup>
+              <FormGroup>
+                <Label for="reason"> Motivo de la entrega</Label>
+                <Input type="textarea" className={`${errors.reason} && 'danger', "form-group"`} name="reason" id='reason' onChange={handleInputChange} />
+              </FormGroup>
+              <FormGroup>
+                <Label for="contact">Contacto</Label>
+                <Input type="textarea" className={`${errors.contact} && 'danger', "form-group"`} name="contact" id='contact' onChange={handleInputChange} />
+              </FormGroup>
+              <FormGroup>
+                <Label for="province">Provincia Donde se Encuentra</Label>
+                <Input type="textarea" className={`${errors.province} && 'danger', "form-group"`} name="province" id='province' onChange={handleInputChange} />
+              </FormGroup>
+              <FormGroup>
+                <Label><strong>Sube Una Foto Para Que Todos Puedan Verlo!!</strong></Label>
+                <FormGroup>
+                  <Input type='file' name='file' onChange={handleChangeImage} />
+                </FormGroup>
+              </FormGroup>
 
-     </ModalBody>
-     <ModalFooter>
+            </ModalBody>
+            <ModalFooter>
 
 
-   {/* {errors.reason ? <Button color="danger" onClick={toggleAdd} disabled>Enviar</Button> :
+              {/* {errors.reason ? <Button color="danger" onClick={toggleAdd} disabled>Enviar</Button> :
          <Button className={styles.button_} type='submit' onClick={() => { handleAddReview(idProductAdding) }}
          >Añadir Reseña</Button>}  */}
 
-       <Button className={styles.button_} onClick={handleCreate}>Enviar</Button>
-       <Button className={styles.button_} onClick={toggleAdd}>Salir</Button>
-     </ModalFooter>
-   </Form>
- </Modal>
- </div>
-      </Fragment>
-    )
+              <Button className="button_adop" onClick={handleCreate}>Enviar</Button>
+              <Button className="button_adop" onClick={toggleAdd}>Salir</Button>
+            </ModalFooter>
+          </Form>
+        </Modal>
+      </div>
+    </Fragment>
+  )
 
 }
 
 
 
 
- 
+
 
 
 
